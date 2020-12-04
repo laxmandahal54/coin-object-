52c6142fbc022599e3f1e77622e4b04ecbc153dc
laxmandahal54 <69312479+laxmandahal54@users.noreply.github.com>
2020-12-04 14:31:13 -0500

    thanks for helping out even in off day

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ let coin = {
     return this.state;
   },
 
-  toString: function () {
-    return "head";
-    let display20Images = document.createElement(ima);
-    if (this.state === 1) {
-      image.src = "./image/heads25.pmg";
-    } else {
-      return "tell";
-      image.src = "./image/tells25.jpg";
+  tostring: function () {
+    if (this.state === 0) {
+      return "heads";
     }
 
+    if (this.state === 1) {
+      return "tails";
+    }
     /* 2. Return the string "Heads" or "Tails", depending on whether
-           "this.state" is 0 or 1. */
+       "this.state" is 0 or 1. */
   },
 
   // console.log(coin.tostring());
 
   toHTML: function () {
     let image = document.createElement("img");
+    image.style.width = "150px";
+    image.style.height = "100px";
+
     if (this.state === 0) {
       image.src = "./image/heads25.png";
     } else {
@@ -42,12 +43,19 @@ let coin = {
 let paragraph = document.createElement("img");
 coin.flip();
 console.log(coin);
-document.body.append(coin.toString());
+document.body.append(coin.tostring());
 document.body.append(coin.toHTML());
-function display20flips() {
-  /*  uses a loop to flip the coin 20 times. 
+
+/*  uses a loop to flip the coin 20 times. 
 Each time the coin flips, display the result of each 
 flip as a string on the page (make use of your toString() method)*/
+function display20flips() {
+  for (let i = 0; i < 20; i++) {
+    coin.flip();
+    let li = document.createElement("li");
+    li.innerHTML = coin.tostring();
+    document.body.append(li);
+  }
 }
 
 function display20images() {
@@ -55,4 +63,12 @@ function display20images() {
 but this time instead of displaying the result as a string,
  display the result of each flip as an 
 HTML IMG element on the page (make use of your toHTML() method).*/
+
+  for (let i = 0; i < 20; i++) {
+    coin.flip();
+    document.body.append(coin.toHTML());
+  }
 }
+
+display20flips();
+display20images();
